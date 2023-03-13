9ca73dcd15abc1f64feeda925901934326978cf5
Manish Luste <59191348+manish24-tech@users.noreply.github.com>
2023-03-13 14:26:04 +0530

    apps module in src

---
+++ practice-app/src/app/app-routing.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './theme/layout/admin/admin.component';
+import {AuthComponent} from './theme/layout/auth/auth.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard/analytics',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./demo/dashboard/dashboard.module').then(module => module.DashboardModule)
+      },
+      {
+        path: 'layout',
+        loadChildren: () => import('./demo/pages/layout/layout.module').then(module => module.LayoutModule)
+      },
+      {
+        path: 'sample-page',
+        loadChildren: () => import('./demo/pages/sample-page/sample-page.module').then(module => module.SamplePageModule)
+      }
+    ]
+  },
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      {
+        path: 'auth',
+        loadChildren: () => import('./demo/pages/authentication/authentication.module').then(module => module.AuthenticationModule)
+      },
+      {
+        path: 'maintenance',
+        loadChildren: () => import('./demo/pages/maintenance/maintenance.module').then(module => module.MaintenanceModule)
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ practice-app/src/app/app.module.ts
@@ -0,0 +1,68 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { authInterceptorProviders } from './_helpers/auth.interceptor';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './theme/shared/shared.module';
+
+import { AppComponent } from './app.component';
+import { AdminComponent } from './theme/layout/admin/admin.component';
+import { AuthComponent } from './theme/layout/auth/auth.component';
+import { NavigationComponent } from './theme/layout/admin/navigation/navigation.component';
+import { NavContentComponent } from './theme/layout/admin/navigation/nav-content/nav-content.component';
+import { NavGroupComponent } from './theme/layout/admin/navigation/nav-content/nav-group/nav-group.component';
+import { NavCollapseComponent } from './theme/layout/admin/navigation/nav-content/nav-collapse/nav-collapse.component';
+import { NavItemComponent } from './theme/layout/admin/navigation/nav-content/nav-item/nav-item.component';
+import { NavBarComponent } from './theme/layout/admin/nav-bar/nav-bar.component';
+import { NavLeftComponent } from './theme/layout/admin/nav-bar/nav-left/nav-left.component';
+import { NavSearchComponent } from './theme/layout/admin/nav-bar/nav-left/nav-search/nav-search.component';
+import { NavRightComponent } from './theme/layout/admin/nav-bar/nav-right/nav-right.component';
+import { ConfigurationComponent } from './theme/layout/admin/configuration/configuration.component';
+
+import { ToggleFullScreenDirective } from './theme/shared/full-screen/toggle-full-screen';
+
+/* Menu Items */
+import { NavigationItem } from './theme/layout/admin/navigation/navigation';
+import { NgbButtonsModule, NgbDropdownModule, NgbTabsetModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+const APP_MODULE = [
+  BrowserModule,
+  CommonModule,
+  FormsModule,
+  ReactiveFormsModule,
+  AppRoutingModule,
+  BrowserAnimationsModule,
+  SharedModule,
+  NgbDropdownModule,
+  NgbTooltipModule,
+  NgbButtonsModule,
+  NgbTabsetModule,
+  HttpClientModule
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    AuthComponent,
+    NavigationComponent,
+    NavContentComponent,
+    NavGroupComponent,
+    NavCollapseComponent,
+    NavItemComponent,
+    NavBarComponent,
+    NavLeftComponent,
+    NavSearchComponent,
+    NavRightComponent,
+    ConfigurationComponent,
+    ToggleFullScreenDirective
+  ],
+  imports: APP_MODULE,
+  providers: [NavigationItem, authInterceptorProviders],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
